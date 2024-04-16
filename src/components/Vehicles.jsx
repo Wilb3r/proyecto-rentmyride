a3c8a789929488912dc38e33a7e1ca81591bc96f
Wilber Alfaro <Wilberalfaro72@gmail.com>
2024-04-15 21:23:39 -0600

    Agregando Vehiculos del Json

--- src/components/Vehicles.jsx
+++ src/components/Vehicles.jsx
@@ -1,8 +1,29 @@
-import React from "react";
+import '../css/Cars.css'
+import React, { useState} from "react";
+import { cars as initialCars } from "../mocks/cars.json"
 
  const Vehicles = () => {
+    const [cars] = useState(initialCars);
     return (
-        <h1>Hola desde Vehicles</h1>
+        <main className="cars">
+            <ul>
+                {
+                    cars.slice(0,9).map((car) => {
+
+                        return (
+                            <li key={car.id}>
+                                <img src={car.thumbnail} alt={car.Manufacturer} />
+                                <div>
+                                    <strong>
+                                        {car.Manufacturer} - ${car.price*0.004}
+                                    </strong>
+                                </div>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </main>
     )
  };
 
