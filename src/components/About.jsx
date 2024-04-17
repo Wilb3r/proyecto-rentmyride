84cb43dcb997c8519d8b95246430edc3a583f37b
Josseline Llanes <garciajeannettell32@gmail.com>
2024-04-16 20:56:13 -0600

    cambios en las pestañas

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React from 'react';
+import '../css/About.css';
 
- const About = () => {
+function About() {
     return (
-        <h1>Hola desde About</h1>
-    )
- };
+        <div className="about-container">
+            <h2>Acerca de Nosotros</h2>
+            <div className="about-content">
+                <p>
+                    En <strong>RentMyRide</strong>, nos apasiona proporcionar a nuestros clientes la libertad de explorar el mundo a su propio ritmo. Desde nuestra fundación en 2020, nos hemos dedicado a ofrecer un servicio de alquiler de coches excepcional y una experiencia inigualable en cada viaje.
+                </p>
+                <h3>Nuestra Historia</h3>
+                <p>
+                    Nuestra historia comenzó con una visión simple: facilitar el acceso a vehículos confiables para aquellos que desean viajar con comodidad y estilo. 
+                </p>
+                <h3>Nuestra Misión</h3>
+                <p>
+                    En <strong>RentMyRide</strong>, nuestra misión es ofrecer a nuestros clientes una experiencia de alquiler de coches sin complicaciones.
+                </p>
+                <h3>Nuestro Compromiso</h3>
+                <p>
+                    Nuestro compromiso con la satisfacción del cliente es nuestra máxima prioridad. Desde el momento en que haces tu reserva hasta el momento en que devuelves.
+                </p>
+                <h3>Únete a Nosotros</h3>
+                <p>
+                    Ya sea que estés planeando un viaje de negocios, unas vacaciones en familia o una escapada de fin de semana, en <strong>RentMyRide</strong> estamos aquí para ayudarte a llegar a tu destino con facilidad. ¡Únete a nosotros y descubre la libertad de viajar a tu propio ritmo!
+                </p>
+            </div>
+        </div>
+    );
+}
 
- export default About;
+export default About;
--- src/components/Constact.jsx
+++ src/components/Constact.jsx
@@ -1,9 +1,67 @@
-import React from "react";
+import React, { useState } from 'react';
+import '../css/Constact.css'; 
 
- const Contact = () => {
+function Constact() {
+    const [showForm, setShowForm] = useState(false);
+
+    const toggleForm = () => {
+        setShowForm(!showForm);
+    };
+
     return (
-        <h1>Hola desde Contact</h1>
-    )
- };
+        <div className="contact-container">
+            <h2>Contáctanos</h2>
+            <p>¡Estamos aquí para ayudarte! No dudes en ponerte en contacto con nosotros si tienes alguna pregunta o necesitas asistencia.</p>
 
- export default Contact;
+            <p><strong>Teléfono:</strong> <a href="#">+503 2345-6789</a></p>
+            <p><strong>Gmail:</strong> <a href="#">rentmyride@gmail.com</a></p>
+
+            <p><strong>Redes Sociales:</strong></p>
+            <ul>
+                <li><a href="#">Facebook</a></li>
+                <li><a href="#">X</a></li>
+                <li><a href="#">Instagram</a></li>
+            </ul>
+
+            <button onClick={toggleForm} className="contact-form-toggle">Abrir Formulario</button>
+
+            {showForm && (
+                <div className="contact-form-overlay">
+                    <div className="contact-form">
+                        <h3>Formulario de Contacto</h3>
+                        <form className="horizontal-form">
+                        &nbsp; 
+                            <div className="form-group">
+                                <label htmlFor="name">Nombre:</label>
+                                &nbsp; 
+                                <input type="text" id="name" name="name" />
+                            </div>
+                            &nbsp; 
+                            <div className="form-group">
+                                <label htmlFor="email">Gmail:</label>
+                                &nbsp; 
+                                <input type="email" id="email" name="email" />
+                            </div>
+                            &nbsp; 
+                            <div className="form-group">
+                                <label htmlFor="message">Mensaje:</label>
+                                &nbsp; 
+                                <textarea id="message" name="message"></textarea>
+                            </div>
+                            &nbsp; 
+                            <div className="button-group">
+                                <button type="submit">Enviar</button>
+                                &nbsp; 
+                                <button onClick={toggleForm} className="close-form-button">Cerrar</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            )}
+
+            <p>¡Síguenos en las redes sociales para obtener las últimas actualizaciones, promociones especiales y más!</p>
+        </div>
+    );
+}
+
+ export default Constact;
