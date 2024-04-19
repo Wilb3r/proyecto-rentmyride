c7b8361e41e1708c2ed11cabef474d4dae10e806
Ruth Nolasco <nolascoargueta1318@gmail.com>
2024-04-19 14:28:07 -0600

    Solucionando errores

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./css/index.css";
 import "./css/App.css";
 import { BrowserRouter } from "react-router-dom";
+import { FiltersProvider } from "./context/filters.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <div className="container">
     <BrowserRouter>
-      <App />
+      <FiltersProvider>
+        <App />
+      </FiltersProvider>
     </BrowserRouter>
     ,
   </div>
